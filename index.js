b45809a8d33b524f936647d55595d5cd86a5ac27
Ankit Pramanik <pramanik.85849@gmail.com>
2020-08-08 16:53:22 +0530

    first instance

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+
+app.get('/', (req, res) => {
+    res.status(200).send('Made by Ankit');
+});
+
+server.listen(3000);
