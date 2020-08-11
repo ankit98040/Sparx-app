6a5609351717c267319fb37e17934ed601bf4c30
Ankit Pramanik <pramanik.85849@gmail.com>
2020-08-11 23:12:48 +0530

    public scripts created

--- index.js
+++ index.js
@@ -4,9 +4,14 @@ const server = require('http').Server(app);
 const { v4: uuidv4 } = require('uuid');
 
 app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.render('room');
+    res.redirect(`/${uuidv4()}`);
+});
+
+app.get('/:room', (req, res) => {
+    res.render('room', { roomId: req.params.room });
 });
 
 server.listen(3000);
